caf79f6ece734c2f627a9f592405120d9e80d98b
Francisco Omar Franco Espinosa <203403@ids.upchiapas.edu.mx>
2022-07-25 05:02:47 -0500

    add files php to conect with DB

---
+++ conexion.php
@@ -0,0 +1,10 @@
+<?php
+ 
+        $user = "root";
+        $pass = "123456";
+        $server = "192.168.1.155";
+        $db ="plant_check";
+        $con = mysqli_connect($server, $user, $pass, $db);
+
+
+?>
