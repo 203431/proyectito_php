f6ae8985e499b53a3fd5fbf16b92f2715ece3e33
Francisco Omar Franco Espinosa <203403@ids.upchiapas.edu.mx>
2022-07-25 05:03:40 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
  
         $user = "root";
-        $pass = "123456";
-        $server = "192.168.1.155";
+        $pass = "";
+        $server = "192.168.1.86";
         $db ="plant_check";
         $con = mysqli_connect($server, $user, $pass, $db);
 
